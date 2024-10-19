6938d4f28a8390ec0272edb134658db3f6842f92
YangTaeWook <didxodnr0769@gmail.com>
2024-10-19 17:55:40 +0900

    style : client - 기본 UI 스타일 적용  - tailwind css 적용  - 기본 스타일 적용

---
+++ client/src/laytout/ChatLayout.tsx
@@ -0,0 +1,66 @@
+const ChatLayout = () => {
+  return (
+    <div className="mx-auto my-[70px] relative max-w-md [&>*:nth-child(even)]:ml-auto">
+      <div className="w-max max-w-sm m-4 px-4 py-2 rounded-full rounded-bl-none bg-gray-200">
+        hello world
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        my name is not world
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 rounded-2xl rounded-bl-none bg-gray-200">
+        what is it then
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        barbara
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 rounded-2xl rounded-bl-none bg-gray-200">
+        why do you hate me, barbara
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        you just seem really annoying, honestly
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 rounded-2xl rounded-bl-none bg-gray-200">
+        i can change
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        don't lie to me, we are done
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 rounded-2xl rounded-bl-none bg-gray-200">
+        i will leave you alone but you must know that the mitochondria is the
+        powerhouse of the cell
+      </div>
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>{" "}
+      <div className="w-max max-w-sm m-4 px-4 py-2 text-white rounded-2xl rounded-br-none bg-pink-500">
+        i said: we are done
+      </div>
+    </div>
+  );
+};
+
+export default ChatLayout;
---
+++ client/src/laytout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-screen z-10">
+      <form>
+        <div className="flex  h-[50px] items-end py-2 px-3 bg-gray-50 dark:bg-gray-700"></div>
+      </form>
+    </div>
+  );
+};
+
+export default Header;
