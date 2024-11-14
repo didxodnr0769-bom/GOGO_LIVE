2d5a90da7374f48b1df21f7b0b2a11110dfa00f5
YangTaeWook <didxodnr0769@gmail.com>
2024-11-14 19:58:40 +0900

    feat : client/server - 서버 공지 메시지 기능 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,8 +20,8 @@ const io = new Server(server, {
 app.use("/user", user);
 app.use("/chat", chat);
 
-const waitQueue: UserInterface[] = [];
-const roomList: RoomInterface[] = [];
+let waitQueue: UserInterface[] = [];
+let roomList: RoomInterface[] = [];
 
 // 클라이언트 소켓이 연결되었을 때
 io.on("connection", (socket) => {
@@ -30,11 +30,36 @@ io.on("connection", (socket) => {
   // 특정 방에 메시지를 보낼 때
   socket.on("message", ({ room, message }) => {
     const time = new Date().toLocaleTimeString();
-    io.to(room).emit("message", { message, time, sender: socket.id });
+    io.to(room).emit("message", {
+      message,
+      time,
+      sender: socket.id,
+      type: "normal",
+    });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  // disconnect 이벤트 핸들러
+  socket.on("disconnecting", () => {
+    const disconnectRoom = Array.from(socket.rooms)[1];
+    const time = new Date().toLocaleTimeString();
+
+    // 연결된 룸이 있을 경우
+    if (disconnectRoom) {
+      io.to(disconnectRoom).emit("message", {
+        message: "상대방이 연결을 종료하였습니다.",
+        time,
+        sender: "Server",
+        type: "notice",
+      });
+      // 룸 제거
+      removeRoom(disconnectRoom);
+    }
+    // 연결된 룸이 없을 경우
+    else {
+      waitQueue = waitQueue.filter(
+        (waitSocketInfo) => waitSocketInfo.socket !== socket
+      );
+    }
   });
 });
 
@@ -48,6 +73,12 @@ const createRoom = (userList: UserInterface[]) => {
   userList.forEach((user) => {
     user.socket.emit("join room", roomId);
     user.socket.join(roomId);
+    user.socket.emit("message", {
+      message: "채팅에 입장되었습니다.",
+      time: new Date().toLocaleTimeString(),
+      sender: "Server",
+      type: "notice",
+    });
   });
   roomList.push(room);
 };
@@ -60,7 +91,6 @@ const removeRoom = (roomId: string) => {
 // 5초마다 방 생성
 setInterval(() => {
   if (waitQueue.length >= 2) {
-    console.log("ytw room...방생성됨", waitQueue.length);
     const userList = waitQueue.splice(0, 2);
     createRoom(userList);
   }
